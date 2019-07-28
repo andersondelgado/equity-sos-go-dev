c2152c02df627e9df3d9a95c963364954b8fa747
anderson delgado <anderson.andersondelgado.delga@gmail.com>
2019-07-28 17:40:17 -0400

    m

--- controller/userKyc/main.go
+++ controller/userKyc/main.go
@@ -7,9 +7,9 @@ import (
 	"time"
 	//"strconv"
 
-	"../../config"
-	"../../model"
-	"../../util"
+	"github.com/andersondelgado/equity-sos-go-dev/config"
+	"github.com/andersondelgado/equity-sos-go-dev/model"
+	"github.com/andersondelgado/equity-sos-go-dev/util"
 	"github.com/gin-gonic/gin"
 	//"github.com/timjacobi/go-couchdb"
 )
@@ -17,14 +17,14 @@ import (
 // Documents KYC //
 func SelectKyc(c *gin.Context) {
 	var rol util.Rol
-		rol.Acl = "kyc_user"
+	rol.Acl = "kyc_user"
 	var datas util.Response
-		datas = util.Response{
-			false,
-			"error_exception",
-			nil,
-		}
-		
+	datas = util.Response{
+		false,
+		"error_exception",
+		nil,
+	}
+
 	if util.IsRead(c, rol).Success == true {
 		var arrKey = []string{"kyc", "_id", "_rev"}
 
@@ -37,7 +37,7 @@ func SelectKyc(c *gin.Context) {
 
 		//fmt.Println(query)
 		respText := util.FindDataAll(query)
-		
+
 		/*fmt.Println(respText)
 		fmt.Println("*************************\n")*/
 
@@ -68,10 +68,10 @@ func SelectKyc(c *gin.Context) {
 				ts,
 			}
 		}
-		
+
 	} else {
 		datas = util.IsRead(c, rol)
-		
+
 	}
 	c.JSON(200, datas)
 }
@@ -87,19 +87,18 @@ func SelectKycUser(c *gin.Context) {
 
 		var arrKey = []string{"kyc_user", "_id", "_rev"}
 		UserID := c.Param("id")
-		if(UserID == ""){
+		if (UserID == "") {
 			user := util.Auth(c)
-			UserID =  user.ID
+			UserID = user.ID
 		}
 		fmt.Println(UserID)
-		
-		query := map[string]interface{}{
-				"selector": map[string]interface{}{
-					"meta": arrKey[0],
-					"kyc_user.user_id": UserID,
-				},
-			}
 
+		query := map[string]interface{}{
+			"selector": map[string]interface{}{
+				"meta":             arrKey[0],
+				"kyc_user.user_id": UserID,
+			},
+		}
 
 		str, _ := json.Marshal(query)
 		decodex := []byte(str)
@@ -112,27 +111,27 @@ func SelectKycUser(c *gin.Context) {
 		decode := []byte(jsonToString)
 		var results model.KYCUserDocumentsArray
 		json.Unmarshal(decode, &results)
-		
-		var ts model.KycUser 
+
+		var ts model.KycUser
 		for i := range results.Doc {
 			ts = model.KycUser{
-				IDs:         		results.Doc[i].ID,
-				Rev:         		results.Doc[i].Rev,
-				ID:          		results.Doc[i].ID,
-				UserID:         	results.Doc[i].KycUser.UserID,
-				CompanyName:    	results.Doc[i].KycUser.CompanyName,
-				CompanyCountryID:   results.Doc[i].KycUser.CompanyCountryID,
-				CountryID:     		results.Doc[i].KycUser.CountryID,
-				Name: 				results.Doc[i].KycUser.Name,
-				LastName:       	results.Doc[i].KycUser.LastName,
-				City: 				results.Doc[i].KycUser.City,
-				Address:       		results.Doc[i].KycUser.Address,
-				AboutPerson:       	results.Doc[i].KycUser.AboutPerson,
-				PostalCode:       	results.Doc[i].KycUser.PostalCode,
-				Dob:       			results.Doc[i].KycUser.Dob,
-				Attachment:     	results.Doc[i].KycUser.Attachment,
-				CreatedAt:       	results.Doc[i].KycUser.CreatedAt,
-				UpdatedAt:       	results.Doc[i].KycUser.UpdatedAt,
+				IDs:              results.Doc[i].ID,
+				Rev:              results.Doc[i].Rev,
+				ID:               results.Doc[i].ID,
+				UserID:           results.Doc[i].KycUser.UserID,
+				CompanyName:      results.Doc[i].KycUser.CompanyName,
+				CompanyCountryID: results.Doc[i].KycUser.CompanyCountryID,
+				CountryID:        results.Doc[i].KycUser.CountryID,
+				Name:             results.Doc[i].KycUser.Name,
+				LastName:         results.Doc[i].KycUser.LastName,
+				City:             results.Doc[i].KycUser.City,
+				Address:          results.Doc[i].KycUser.Address,
+				AboutPerson:      results.Doc[i].KycUser.AboutPerson,
+				PostalCode:       results.Doc[i].KycUser.PostalCode,
+				Dob:              results.Doc[i].KycUser.Dob,
+				Attachment:       results.Doc[i].KycUser.Attachment,
+				CreatedAt:        results.Doc[i].KycUser.CreatedAt,
+				UpdatedAt:        results.Doc[i].KycUser.UpdatedAt,
 			}
 		}
 
@@ -163,17 +162,17 @@ func AddKycUser(c *gin.Context) {
 	if util.IsCreate(c, rol).Success == true {
 
 		cloudantUrl := config.StrNoSQLDrive()
-		cloudant := util.CloudantDefault()
-		dbName := config.StrNoSQLDBname()
+		//cloudant := util.CloudantDefault()
+		//dbName := config.StrNoSQLDBname()
 
 		if cloudantUrl == "" {
 			c.JSON(200, gin.H{})
 			return
 		}
-		
+
 		var (
 			//datas util.Response
-			t     model.KycUser
+			t model.KycUser
 		)
 		if c.BindJSON(&t) == nil {
 			if t.Name == "" {
@@ -187,25 +186,26 @@ func AddKycUser(c *gin.Context) {
 				date := time.Now()
 
 				t = model.KycUser{
-					UserID:         	user.ID,
-					CompanyName:    	t.CompanyName,
-					CompanyCountryID:   t.CompanyCountryID,
-					CountryID:     		t.CountryID,
-					Name: 				t.Name,
-					LastName:       	t.LastName,
-					City: 				t.City,
-					Address:       		t.Address,
-					AboutPerson:       	t.AboutPerson,
-					PostalCode:       	t.PostalCode,
-					Dob:       			t.Dob,
-					Attachment:     	t.Attachment,
-					CreatedAt:       	date,
-					UpdatedAt:       	date,
+					UserID:           user.ID,
+					CompanyName:      t.CompanyName,
+					CompanyCountryID: t.CompanyCountryID,
+					CountryID:        t.CountryID,
+					Name:             t.Name,
+					LastName:         t.LastName,
+					City:             t.City,
+					Address:          t.Address,
+					AboutPerson:      t.AboutPerson,
+					PostalCode:       t.PostalCode,
+					Dob:              t.Dob,
+					Attachment:       t.Attachment,
+					CreatedAt:        date,
+					UpdatedAt:        date,
 				}
 
 				var arrKey = []string{"kyc_user"}
 
-				cloudant.DB(dbName).Post(map[string]interface{}{"meta": arrKey[0], "tag": arrKey, "kyc_user": t})
+				//cloudant.DB(dbName).Post(map[string]interface{}{"meta": arrKey[0], "tag": arrKey, "kyc_user": t})
+				util.PostCouchDB(map[string]interface{}{"meta": arrKey[0], "tag": arrKey, "kyc_user": t})
 				datas = util.Response{
 					true,
 					"ok",
@@ -233,7 +233,7 @@ func EditKycUser(c *gin.Context) {
 	if util.IsEdit(c, rol).Success == true {
 
 		var (
-			t     model.KycUser
+			t model.KycUser
 		)
 		if c.BindJSON(&t) == nil {
 			if t.Name == "" {
@@ -260,11 +260,11 @@ func EditKycUser(c *gin.Context) {
 				decode := []byte(jsonToString)
 				var result model.KYCUserDocumentsArray
 				json.Unmarshal(decode, &result)
-				results := result//.Doc[0]
+				results := result //.Doc[0]
 
-			fmt.Println("********************************")
-			fmt.Println(results)
-			fmt.Println("********************************")
+				fmt.Println("********************************")
+				fmt.Println(results)
+				fmt.Println("********************************")
 			}
 		}
 
@@ -292,15 +292,15 @@ func KycFaker(c *gin.Context) {
 
 	for i := range result0 {
 		t := model.KYC{
-			Name:         	result0[i].Name,
-			Description:  	result0[i].Description,
-			CreatedAt:  	result0[i].CreatedAt,
-			UpdatedAt: 		result0[i].UpdatedAt,
+			Name:        result0[i].Name,
+			Description: result0[i].Description,
+			CreatedAt:   result0[i].CreatedAt,
+			UpdatedAt:   result0[i].UpdatedAt,
 		}
 		strResponse0 := util.CurlPost(t, "/api/kyc/bulk")
 		fmt.Println("##strResponse0: ", string(strResponse0))
 	}
-	
+
 	datas := util.Response{
 		true,
 		"ok",
@@ -311,10 +311,10 @@ func KycFaker(c *gin.Context) {
 
 func BulkKyc(c *gin.Context) {
 	cloudantUrl := config.StrNoSQLDrive()
-	cloudant := util.CloudantDefault()
+	//cloudant := util.CloudantDefault()
+	//
+	//dbName := config.StrNoSQLDBname()
 
-	dbName := config.StrNoSQLDBname()
-	
 	if cloudantUrl == "" {
 		c.JSON(200, gin.H{})
 		return
@@ -336,8 +336,8 @@ func BulkKyc(c *gin.Context) {
 			c.JSON(200, datas)
 		} else {
 			var arrKey = []string{"kyc"}
-			cloudant.DB(dbName).Post(map[string]interface{}{"meta": arrKey[0], "tag": arrKey, "kyc": t})
-
+			//cloudant.DB(dbName).Post(map[string]interface{}{"meta": arrKey[0], "tag": arrKey, "kyc": t})
+			util.PostCouchDB(map[string]interface{}{"meta": arrKey[0], "tag": arrKey, "kyc": t})
 			datas = util.Response{
 				true,
 				"ok",
